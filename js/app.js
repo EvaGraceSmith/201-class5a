5046e693cb36a10895251133405da882305435e2
EvaGraceSmith <smithevagrace@gmail.com>
2022-11-12 18:10:55 -0600

    finished qFour

--- js/app.js
+++ js/app.js
@@ -105,7 +105,15 @@ let testArray = [2, 3, 4]; //eslint-disable-line
 function sumArray(sumArr) { //eslint-disable-line
   let sumArraySum = sum(sumArr[0],sum(sumArr[1],sumArr[2])[0]);
   console.log(sumArraySum[0]);
+  let message3 = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumArraySum[0]} is their sum.`;
+  console.log(message3);
+
+  return [sumArraySum[0], message3];
+
+  // let productNumber = multiply (multiply(a,b)[0] , c)[0];
 }
+
+
 
 sumArray(testArray);
 
@@ -114,7 +122,7 @@ sumArray(testArray);
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
